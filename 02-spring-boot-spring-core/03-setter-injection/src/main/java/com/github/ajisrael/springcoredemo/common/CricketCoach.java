f319845d62e5c0873470677445e8d95eae0ba8b9
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-02-23 03:39:19 -0500

    A!!  - setup project for setter injection demo

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/github/ajisrael/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.github.ajisrael.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes :-)";
+    }
+}
---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/github/ajisrael/springcoredemo/rest/DemoController.java
@@ -0,0 +1,19 @@
+package com.github.ajisrael.springcoredemo.rest;
+
+import com.github.ajisrael.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
