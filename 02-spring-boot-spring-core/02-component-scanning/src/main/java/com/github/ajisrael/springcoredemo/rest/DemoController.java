de24201e32d3dfa7ba4e3b7dfbf3006edad209b9
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-02-23 03:20:27 -0500

    f   - moved classes into sub packages to show default component scanning

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/github/ajisrael/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.github.ajisrael.springcoredemo.rest;
+
+import com.github.ajisrael.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define constructor for dependency injection
+    @Autowired
+    public DemoController(Coach coach) {
+        myCoach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
