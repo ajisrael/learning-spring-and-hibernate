8cfe51ea92a69d0660ad254e3017182ec3bc452d
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-04-04 11:45:40 -0400

    f   - completed qualifier annotation lesson

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/github/ajisrael/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.github.ajisrael.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
