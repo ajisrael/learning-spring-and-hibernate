3acf26afa2293f26f3ba4cf343496cc4ca12b018
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-04-04 12:26:10 -0400

    r   - switched back to qualifier annotation

--- 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/github/ajisrael/springcoredemo/common/TrackCoach.java
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/github/ajisrael/springcoredemo/common/TrackCoach.java
@@ -1,10 +1,8 @@
 package com.github.ajisrael.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class TrackCoach implements Coach {
 
     @Override
