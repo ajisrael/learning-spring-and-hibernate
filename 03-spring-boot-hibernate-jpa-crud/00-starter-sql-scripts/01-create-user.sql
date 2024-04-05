918ee16fb486e4ac0c415a693df02f4a0ce27a40
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-04-05 09:33:58 -0400

    f   - added comments for process of running commands directly in container

--- 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/01-create-user.sql
+++ 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/01-create-user.sql
@@ -1,3 +1,6 @@
+-- Had to run commands directly in terminal on container to work
+-- Login command (will be prompted to give password)
+mysql -u root -p
 -- Drop user first if they exist
 DROP USER if exists 'springstudent'@'%' ;
 
@@ -5,3 +8,8 @@ DROP USER if exists 'springstudent'@'%' ;
 CREATE USER 'springstudent'@'%' IDENTIFIED BY 'Spring-student123';
 
 GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'%';
+
+-- Check user exists
+select user from mysql.user;
+
+-- May need to refresh connection in DBeaver to see changes
