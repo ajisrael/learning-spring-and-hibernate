615d2cad01944a2335b9fae67a8e19725721a3f3
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-04-05 12:07:30 -0400

    f   - saved student to database

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ajisrael/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ajisrael/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.ajisrael.cruddemo;
 
+import com.ajisrael.cruddemo.dao.StudentDAO;
+import com.ajisrael.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,24 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("John", "Doe", "john@example.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display the id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 }
