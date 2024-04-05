bfe375912d449f4a64ca4b05c5f13e0f2fb99057
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-04-05 12:41:08 -0400

    f   - added query to get all students

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ajisrael/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ajisrael/cruddemo/dao/StudentDAO.java
@@ -2,9 +2,13 @@ package com.ajisrael.cruddemo.dao;
 
 import com.ajisrael.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
