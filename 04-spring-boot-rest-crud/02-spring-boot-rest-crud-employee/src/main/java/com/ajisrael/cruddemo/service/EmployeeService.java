61a3bf19019638b54f6f9f69306bc8624d8ee1dc
ajisrael <43039187+ajisrael@users.noreply.github.com>
2024-05-06 12:24:11 -0400

    f   - added employee service layer

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ajisrael/cruddemo/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.ajisrael.cruddemo.service;
+
+import com.ajisrael.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+}
