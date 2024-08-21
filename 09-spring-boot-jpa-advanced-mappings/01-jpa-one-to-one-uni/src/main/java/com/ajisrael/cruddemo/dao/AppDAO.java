1bb0c4aa60067721a2b59a56aca9305baac108e6
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-08-21 13:34:37 -0400

    f   - created and implemented application dao interface

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/ajisrael/cruddemo/dao/AppDAO.java
@@ -0,0 +1,8 @@
+package com.ajisrael.cruddemo.dao;
+
+import com.ajisrael.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor instructor);
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/ajisrael/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,25 @@
+package com.ajisrael.cruddemo.dao;
+
+import com.ajisrael.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class AppDAOImpl implements AppDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+}
