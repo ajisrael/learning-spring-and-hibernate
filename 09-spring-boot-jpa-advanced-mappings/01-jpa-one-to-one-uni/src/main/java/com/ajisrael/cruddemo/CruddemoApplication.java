11622981526bda865821cacc53c411e2a1b558a6
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-08-21 13:48:29 -0400

    f   - completed app setup for saving instructor and constructor detail

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/ajisrael/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/ajisrael/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.ajisrael.cruddemo;
 
+import com.ajisrael.cruddemo.dao.AppDAO;
+import com.ajisrael.cruddemo.entity.Instructor;
+import com.ajisrael.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,30 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
 		return runner -> {
-			System.out.println("Hello World");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+
+		Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "Luv 2 code!!!");
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving instructor: " + tempInstructor);
+
+		// save the instructor
+		//
+		// NOTE: this wil ALSO save the details object
+		// because of CascadeType.ALL
+		//
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!!!");
 	}
 }
